bde21d5769e2f3aaaf6ec65fd2f259a101c33976
SidneySM1 <sidneysilvantl99@gmail.com>
2026-03-12 03:15:28 -0300

    feat: add CustomerPhone and GuestCount properties to ReservationDTO

--- services/shared-contracts/ReservationDTO.cs
+++ services/shared-contracts/ReservationDTO.cs
@@ -2,11 +2,14 @@ namespace SharedContracts;
 
 // Usamos 'record' no lugar de 'class' porque DTOs são apenas para transporte de dados.
 // Records são imutáveis por padrão e perfeitos para viajar em filas de mensageria.
+
 public record ReservationDTO(
     Guid ReservationId,
     string CustomerName,
     string CustomerEmail,
+    string CustomerPhone, // Adicionado para o Guest Checkout
     int TableNumber,
+    int GuestCount,       // Adicionado para sabermos a lotação
     DateTime ReservationDate,
     DateTime CreatedAt
 );
