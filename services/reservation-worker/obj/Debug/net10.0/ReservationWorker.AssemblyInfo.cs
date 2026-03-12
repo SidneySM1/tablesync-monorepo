7e23450fd99e7b001df340e5ed3cc969a9754c07
SidneySM1 <sidneysilvantl99@gmail.com>
2026-03-12 02:39:38 -0300

    feat: integrate SharedContracts project into ApiGateway and ReservationWorker
    - Updated ApiGateway and ReservationWorker projects to reference SharedContracts.
    - Modified project.assets.json to include SharedContracts as a dependency.
    - Updated AssemblyInfo files for both projects to reflect new versioning.
    - Adjusted project.nuget.cache and project.assets.json to ensure proper package management.
    - Added project reference for SharedContracts in ReservationWorker.csproj.

--- services/reservation-worker/obj/Debug/net10.0/ReservationWorker.AssemblyInfo.cs
+++ services/reservation-worker/obj/Debug/net10.0/ReservationWorker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReservationWorker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+416e124b09353939898eae0a16089423b86b98d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ea78c6ebe4fc3272c1af7167877d553164563b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReservationWorker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReservationWorker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
